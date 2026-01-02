beeb37a8b065d0d6f9d8d0bc3032b2b520092a8e
Daini25 <noledaini@gmail.com>
2026-01-02 21:16:29 +0530

    Fix: added eureka anotations

--- student-service/src/main/java/com/freelance/student/service/StudentServiceApplication.java
+++ student-service/src/main/java/com/freelance/student/service/StudentServiceApplication.java
@@ -2,8 +2,10 @@ package com.freelance.student.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class StudentServiceApplication {
 
 	public static void main(String[] args) {
